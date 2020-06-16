b8de4ee7aa64c9e4c235daf8a15c507923b83815
Mark DiValerio <markadivalerio@gmail.com>
2020-06-16 18:30:04 -0500

    final commit of hw1

---
+++ hw1/mad2799_se9727/src/hw1.h
@@ -0,0 +1,11 @@
+#include <vector>
+
+// Returns the euclidian lenght of 'vector'.
+double euclidean_length(std::vector<double> vector);
+
+void parallel_prefix_scan_sum(std::vector<int> &arr);
+
+// Returns a sorted vector that contains all the unique elements in 'sorted_vector'.
+//
+// NOTE: non-const vectors are not thread-safe.
+std::vector<int64_t> discard_duplicates(std::vector<int64_t> sorted_vector);
