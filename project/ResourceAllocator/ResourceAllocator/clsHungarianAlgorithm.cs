6432ac3ebb50ab218e7d787884df6d53c89ebb1a
sanjaikanth <32877146+sanjaikanth@users.noreply.github.com>
2020-08-11 20:37:56 -0500

    Update clsHungarianAlgorithm.cs

--- project/ResourceAllocator/ResourceAllocator/clsHungarianAlgorithm.cs
+++ project/ResourceAllocator/ResourceAllocator/clsHungarianAlgorithm.cs
@@ -1,275 +1,230 @@
-﻿
-/*
-C# hungarian algorithm implementation
-    Copyright (C) 2015  Ivan Jurin
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
-
-
-using System;
 using System.Collections.Generic;
-using System.Threading;
 
 namespace ResourceAllocator
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public sealed class clsHungarianAlgorithm
     {
-       // private Action<string> updaterDelegate;
 
-        private readonly int[,] _costMatrix;
-        private int _inf;
-        private int _n; //number of elements
-        private int[] _lx; //labels for workers
-        private int[] _ly; //labels for jobs 
-        private bool[] _s;
-        private bool[] _t;
-        private int[] _matchX; //vertex matched with x
-        private int[] _matchY; //vertex matched with y
-        private int _maxMatch;
+        private int[] arrVertexMatchX;
+        private int[] arrVertexMatchY;
+        private int MaximumMatch;
         private int[] _slack;
         private int[] _slackx;
-        private int[] _prev; //memorizing paths
+        private int[] PreviousPath;
+        private readonly int[,] MatrixTable;
+        private int MaxInt;
+        private int NumElements;
+        private int[] WorkersLabel;
+        private int[] JobsLabel;
+        private bool[] _s;
+        private bool[] _t;
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="costMatrix"></param>
-        public clsHungarianAlgorithm(int[,] costMatrix)//, Action<string> theDelegate)
+        public clsHungarianAlgorithm(int[,] costMatrix)
         {
-            //this.updaterDelegate = theDelegate;
-            _costMatrix = costMatrix;
-          
+            MatrixTable = costMatrix;
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
+        private void UpdateWorkerSlackAndJobLabels()
+        {
+            var delta = MaxInt;
+            for (var i = 0; i < NumElements; i++)
+                if (!_t[i])
+                    if (delta > _slack[i])
+                        delta = _slack[i];
+            for (var i = 0; i < NumElements; i++)
+            {
+                if (_s[i])
+                    WorkersLabel[i] = WorkersLabel[i] + delta;
+                if (_t[i])
+                    JobsLabel[i] = JobsLabel[i] - delta;
+                else _slack[i] = _slack[i] - delta;
+            }
+        }
+        //
+        private void populateSlackArray(int x, int prevx)
+        {
+            _s[x] = true;
+            PreviousPath[x] = prevx;
+
+            var lxx = WorkersLabel[x];
+            for (var y = 0; y < NumElements; y++)
+            {
+                if (MatrixTable[x, y] - lxx - JobsLabel[y] >= _slack[y]) continue;
+                _slack[y] = MatrixTable[x, y] - lxx - JobsLabel[y];
+                _slackx[y] = x;
+            }
+        }
+        //
+        private void FillMatchArray()
+        {
+            for (var x = 0; x < NumElements; x++)
+            {
+                for (var y = 0; y < NumElements; y++)
+                {
+                    if (MatrixTable[x, y] != WorkersLabel[x] + JobsLabel[y] || arrVertexMatchY[y] != -1) continue;
+                    arrVertexMatchX[x] = y;
+                    arrVertexMatchY[y] = x;
+                    MaximumMatch++;
+                    break;
+                }
+            }
+        }
+        //This will assign default VertexMatch to -1
+        private void AssignDefaultValues()
+        {
+            for (var i = 0; i < NumElements; i++)
+            {
+                arrVertexMatchX[i] = -1;
+                arrVertexMatchY[i] = -1;
+            }
+        }
+
+        //Thsui will initialise S and T arrray
+        private void InitSt()
+        {
+            for (var i = 0; i < NumElements; i++)
+            {
+                _s[i] = false;
+                _t[i] = false;
+            }
+        }
+        //
+        private void InitializeLabel()
+        {
+            for (var i = 0; i < NumElements; i++)
+            {
+                var minRow = MatrixTable[i, 0];
+                for (var j = 0; j < NumElements; j++)
+                {
+                    if (MatrixTable[i, j] < minRow) minRow = MatrixTable[i, j];
+                    if (minRow == 0) break;
+                }
+                WorkersLabel[i] = minRow;
+            }
+            for (var j = 0; j < NumElements; j++)
+            {
+                var minColumn = MatrixTable[0, j] - WorkersLabel[0];
+                for (var i = 0; i < NumElements; i++)
+                {
+                    if (MatrixTable[i, j] - WorkersLabel[i] < minColumn) minColumn = MatrixTable[i, j] - WorkersLabel[i];
+                    if (minColumn == 0) break;
+                }
+                JobsLabel[j] = minColumn;
+            }
+        }
         public int[] Run()
         {
-            //this.updaterDelegate("Updating from Inside Class");
-            //Thread.Sleep(10 * 1000);
-            _n = _costMatrix.GetLength(0);
+            NumElements = MatrixTable.GetLength(0);
 
-            _lx = new int[_n];
-            _ly = new int[_n];
-            _s = new bool[_n];
-            _t = new bool[_n];
-            _matchX = new int[_n];
-            _matchY = new int[_n];
-            _slack = new int[_n];
-            _slackx = new int[_n];
-            _prev = new int[_n];
-            _inf = int.MaxValue;
+            WorkersLabel = new int[NumElements];
+            JobsLabel = new int[NumElements];
+            _s = new bool[NumElements];
+            _t = new bool[NumElements];
+            arrVertexMatchX = new int[NumElements];
+            arrVertexMatchY = new int[NumElements];
+            _slack = new int[NumElements];
+            _slackx = new int[NumElements];
+            PreviousPath = new int[NumElements];
+            MaxInt = int.MaxValue;
 
 
-            InitMatches();
+            AssignDefaultValues();
 
-            if (_n != _costMatrix.GetLength(1))
+            if (NumElements != MatrixTable.GetLength(1))
                 return null;
 
-            InitLbls();
+            InitializeLabel();
 
-            _maxMatch = 0;
+            MaximumMatch = 0;
 
-            InitialMatching();
+            FillMatchArray();
 
-            var q = new Queue<int>();
+            var queue = new Queue<int>();
 
-            #region augment
 
-            while (_maxMatch != _n)
+
+            while (MaximumMatch != NumElements)
             {
-                q.Clear();
+                queue.Clear();
 
                 InitSt();
-                //Array.Clear(S,0,n);
-                //Array.Clear(T, 0, n);
-
-
                 //parameters for keeping the position of root node and two other nodes
                 var root = 0;
                 int x;
                 var y = 0;
 
                 //find root of the tree
-                for (x = 0; x < _n; x++)
+                for (x = 0; x < NumElements; x++)
                 {
-                    if (_matchX[x] != -1) continue;
-                    q.Enqueue(x);
+                    if (arrVertexMatchX[x] != -1) continue;
+                    queue.Enqueue(x);
                     root = x;
-                    _prev[x] = -2;
+                    PreviousPath[x] = -2;
 
                     _s[x] = true;
                     break;
                 }
 
-                //init slack
-                for (var i = 0; i < _n; i++)
+                for (var i = 0; i < NumElements; i++)
                 {
-                    _slack[i] = _costMatrix[root, i] - _lx[root] - _ly[i];
+                    _slack[i] = MatrixTable[root, i] - WorkersLabel[root] - JobsLabel[i];
                     _slackx[i] = root;
                 }
 
-                //finding augmenting path
+                //Find label path
                 while (true)
                 {
-                    while (q.Count != 0)
+                    while (queue.Count != 0)
                     {
-                        x = q.Dequeue();
-                        var lxx = _lx[x];
-                        for (y = 0; y < _n; y++)
+                        x = queue.Dequeue();
+                        var lxx = WorkersLabel[x];
+                        for (y = 0; y < NumElements; y++)
                         {
-                            if (_costMatrix[x, y] != lxx + _ly[y] || _t[y]) continue;
-                            if (_matchY[y] == -1) break; //augmenting path found!
+                            if (MatrixTable[x, y] != lxx + JobsLabel[y] || _t[y]) continue;
+                            if (arrVertexMatchY[y] == -1) break;
                             _t[y] = true;
-                            q.Enqueue(_matchY[y]);
+                            queue.Enqueue(arrVertexMatchY[y]);
 
-                            AddToTree(_matchY[y], x);
+                            populateSlackArray(arrVertexMatchY[y], x);
                         }
-                        if (y < _n) break; //augmenting path found!
+                        if (y < NumElements) break;
                     }
-                    if (y < _n) break; //augmenting path found!
-                    UpdateLabels(); //augmenting path not found, update labels
+                    if (y < NumElements) break;
+                    UpdateWorkerSlackAndJobLabels();
 
-                    for (y = 0; y < _n; y++)
+                    for (y = 0; y < NumElements; y++)
                     {
-                        //in this cycle we add edges that were added to the equality graph as a
-                        //result of improving the labeling, we add edge (slackx[y], y) to the tree if
-                        //and only if !T[y] &&  slack[y] == 0, also with this edge we add another one
-                        //(y, yx[y]) or augment the matching, if y was exposed
-
                         if (_t[y] || _slack[y] != 0) continue;
-                        if (_matchY[y] == -1) //found exposed vertex-augmenting path exists
+                        if (arrVertexMatchY[y] == -1)
                         {
                             x = _slackx[y];
                             break;
                         }
                         _t[y] = true;
-                        if (_s[_matchY[y]]) continue;
-                        q.Enqueue(_matchY[y]);
-                        AddToTree(_matchY[y], _slackx[y]);
+                        if (_s[arrVertexMatchY[y]]) continue;
+                        queue.Enqueue(arrVertexMatchY[y]);
+                        populateSlackArray(arrVertexMatchY[y], _slackx[y]);
                     }
-                    if (y < _n) break;
+                    if (y < NumElements) break;
                 }
 
-                _maxMatch++;
+                MaximumMatch++;
 
-                //inverse edges along the augmenting path
+                //Reverse Edges
                 int ty;
-                for (int cx = x, cy = y; cx != -2; cx = _prev[cx], cy = ty)
+                for (int cx = x, cy = y; cx != -2; cx = PreviousPath[cx], cy = ty)
                 {
-                    ty = _matchX[cx];
-                    _matchY[cy] = cx;
-                    _matchX[cx] = cy;
+                    ty = arrVertexMatchX[cx];
+                    arrVertexMatchY[cy] = cx;
+                    arrVertexMatchX[cx] = cy;
                 }
             }
 
-            #endregion
-            //this.updaterDelegate("Completed from Inside Class");
-            return _matchX;
-        }
 
-        private void InitMatches()
-        {
-            for (var i = 0; i < _n; i++)
-            {
-                _matchX[i] = -1;
-                _matchY[i] = -1;
-            }
-        }
-
-        private void InitSt()
-        {
-            for (var i = 0; i < _n; i++)
-            {
-                _s[i] = false;
-                _t[i] = false;
-            }
-        }
-
-        private void InitLbls()
-        {
-            for (var i = 0; i < _n; i++)
-            {
-                var minRow = _costMatrix[i, 0];
-                for (var j = 0; j < _n; j++)
-                {
-                    if (_costMatrix[i, j] < minRow) minRow = _costMatrix[i, j];
-                    if (minRow == 0) break;
-                }
-                _lx[i] = minRow;
-            }
-            for (var j = 0; j < _n; j++)
-            {
-                var minColumn = _costMatrix[0, j] - _lx[0];
-                for (var i = 0; i < _n; i++)
-                {
-                    if (_costMatrix[i, j] - _lx[i] < minColumn) minColumn = _costMatrix[i, j] - _lx[i];
-                    if (minColumn == 0) break;
-                }
-                _ly[j] = minColumn;
-            }
-        }
-
-        private void UpdateLabels()
-        {
-            var delta = _inf;
-            for (var i = 0; i < _n; i++)
-                if (!_t[i])
-                    if (delta > _slack[i])
-                        delta = _slack[i];
-            for (var i = 0; i < _n; i++)
-            {
-                if (_s[i])
-                    _lx[i] = _lx[i] + delta;
-                if (_t[i])
-                    _ly[i] = _ly[i] - delta;
-                else _slack[i] = _slack[i] - delta;
-            }
-        }
-
-        private void AddToTree(int x, int prevx)
-        {
-            //x-current vertex, prevx-vertex from x before x in the alternating path,
-            //so we are adding edges (prevx, matchX[x]), (matchX[x],x)
-
-            _s[x] = true; //adding x to S
-            _prev[x] = prevx;
-
-            var lxx = _lx[x];
-            //updateing slack
-            for (var y = 0; y < _n; y++)
-            {
-                if (_costMatrix[x, y] - lxx - _ly[y] >= _slack[y]) continue;
-                _slack[y] = _costMatrix[x, y] - lxx - _ly[y];
-                _slackx[y] = x;
-            }
-        }
-
-        private void InitialMatching()
-        {
-            for (var x = 0; x < _n; x++)
-            {
-                for (var y = 0; y < _n; y++)
-                {
-                    if (_costMatrix[x, y] != _lx[x] + _ly[y] || _matchY[y] != -1) continue;
-                    _matchX[x] = y;
-                    _matchY[y] = x;
-                    _maxMatch++;
-                    break;
-                }
-            }
+            return arrVertexMatchX;
         }
     }
 }
